0abb85f66b43027ffe3f21f510d21d5d62de2575
Thomas LÉVEIL <thomasleveil@gmail.com>
2014-06-27 00:15:40 +0200

    only upgrade Docker is already installed

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,10 +16,16 @@ fi
 
 ## install docker
 if [[ $(groups vagrant | grep -q docker) -ne 0 ]]; then
+	echo "installing Docker"
 	curl -sL https://get.docker.io/ | sh
 	curl -sL https://raw.githubusercontent.com/dotcloud/docker/master/contrib/completion/bash/docker > /etc/bash_completion.d/docker
 	adduser vagrant docker
+else
+	echo "upgrading Docker"
+	apt-get update
+	apt-get -y install docker
 fi
+
 
 ## add color support for cygwin term
 sed -i 's/xterm-color) color_prompt=yes;;/xterm-color|cygwin) color_prompt=yes;;/' /home/vagrant/.bashrc
